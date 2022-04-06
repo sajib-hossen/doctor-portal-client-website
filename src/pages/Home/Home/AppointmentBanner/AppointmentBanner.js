a7b6516f7d41c46e8cf66cb106fd56b0490a3b60
sajib hossen <sajibhossensa90@gmail.com>
2022-04-06 15:49:32 +0600

    new bnnar

---
+++ src/pages/Home/Home/AppointmentBanner/AppointmentBanner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppointmentBanner.css";
+
+const AppointmentBanner = () => {
+  return (
+    <div>
+      <h1>heloooo</h1>
+    </div>
+  );
+};
+
+export default AppointmentBanner;
